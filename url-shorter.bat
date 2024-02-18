638cff16f0dfcd985483100b10df20a1381e1952
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-02-18 04:19:57 +0100

    v0.2.2x
    - further enhanced the "save qr code"

--- url-shorter.bat
+++ url-shorter.bat
@@ -1,20 +1,24 @@
-@echo off && title URL-Shorter_v0.2.2 [ConzZah] rem done @ 2:01 am / 17.02.2024
+@echo off && title URL-Shorter_v0.2.2x [ConzZah] rem done @ 3:05 am / 18.02.2024
 set c2c=%tmp%\copylink2clipboard.txt
 echo  PASTE LINK ^& PRESS ENTER : && echo. && set /p user_url=
-echo. && echo [FETCHING SHORTLINK ^& QR-CODE..] && echo.
+echo. && echo [FETCHING SHORTLINK ^& QR-CODE..]
 curl -s https://v.gd/create.php?format=simple^&url="%user_url%" >>"%c2c%"
-cls && type "%c2c%" && clip < "%c2c%" && del /f /q "%c2c%" && echo. && echo.
-curl https://qrenco.de/"%user_url%"
+cls && echo QR-CODE: && echo -------- && curl https://qrenco.de/"%user_url%" && echo.
+echo SHORTLINK: && echo ---------- && type "%c2c%" && clip < "%c2c%" && del /f /q "%c2c%"
 :saveprompt
-echo. && echo SAVE QR-CODE AS .PNG FILE ? &&echo.
+echo. && echo. && echo SAVE QR-CODE AS .PNG ? && echo ----------------------
+echo [1] + [ENTER] = YES && echo.
 echo [ENTER] = NO && echo.
-echo [1] + [ENTER] = YES
 set /p saveprompt= 
 if "%saveprompt%"=="1" goto saveqrcode
 if not "%saveprompt%"=="1" goto end
 :end
-echo. && echo PRESS [ENTER] TO EXIT. && pause >nul && exit
+echo ### PRESS [ENTER] TO EXIT. ### && pause >nul && exit
 :saveqrcode
-echo. && echo ENTER NAME FOR QR-CODE: && set /p saveqr_name=
-curl -s http://api.qrserver.com/v1/create-qr-code/?data="%user_url%" -o "%saveqr_name%".png
-echo [ QR-CODE SAVED AS: "%saveqr_name%.png" ] && goto end
+set savefolder=url-shorter_QR-CODES
+set savepath=%USERPROFILE%\Downloads\
+cd "%savepath%" && if not exist "%savefolder%" mkdir "%savefolder%"  
+cd "%savefolder%" && echo. && echo ENTER NAME FOR QR-CODE: && set /p saveqr_name=
+curl -s http://api.qrserver.com/v1/create-qr-code/?data="%user_url%" -o "%saveqr_name%".png && echo.
+echo [ QR-CODE SAVED AS: "%saveqr_name%.png" ] 
+echo [ QR-CODE SAVED TO: "%savepath%%savefolder%" ] && echo. && goto end
